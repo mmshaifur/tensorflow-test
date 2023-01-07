0f0724d9a61633e20fa64b201ae02c109dfeba23
Shaifur Rahman <shaifur.cse@gmail.com>
2023-01-06 23:57:34 -0500

    Add files via upload

---
+++ tf-test-gpu.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+(X_train, y_train), (X_test, y_test) = keras.datasets.cifar10.load_data()
+
+# checking images shape
+X_train.shape, X_test.shape
+
+# display single image shape
+X_train[0].shape
+
+# checking labels
+y_train[:5]
+
+
+# scaling image values between 0-1
+X_train_scaled = X_train/255
+X_test_scaled = X_test/255
+
+# one hot encoding labels
+y_train_encoded = keras.utils.to_categorical(y_train, num_classes = 10, dtype = 'float32')
+y_test_encoded = keras.utils.to_categorical(y_test, num_classes = 10, dtype = 'float32')
+
+def get_model():
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(32,32,3)),
+        keras.layers.Dense(3000, activation='relu'),
+        keras.layers.Dense(1000, activation='relu'),
+        keras.layers.Dense(10, activation='sigmoid')    
+    ])
+    model.compile(optimizer='SGD',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+    return model
+    
+  
+# GPU
+with tf.device('/GPU:0'):
+    model_gpu = get_model()
+    model_gpu.fit(X_train_scaled, y_train_encoded, epochs = 100)
